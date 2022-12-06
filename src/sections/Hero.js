0c4ecea7f2bcc8778a1115ec32d58d7ba7f5270e
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2022-12-06 09:44:18 -0500

    Update Hero.js

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -14,7 +14,7 @@ const Hero = () => {
       <div className="relative flex flex-col px-10 mx-auto space-y-5 md:w-3/4">
         <header className="flex items-center justify-between space-x-3">
           <Link href="/">
-            <a className="text-2xl text-violet-500 font-bold">IMTIJAAB</a>
+            <a className="text-2xl text-violet-600 font-bold">IMTIJAAB</a>
           </Link>
           <button className="md:hidden" onClick={toggleMenu}>
             {!showMenu ? (
@@ -32,9 +32,9 @@ const Hero = () => {
             ].join(' ')}
           >
             <nav className="flex flex-col w-full space-x-0 space-y-3 text-center md:space-y-0 md:space-x-3 md:flex-row">
-              <a className="px-5 py-2 rounded text-violet-500 hover:bg-white-100">About</a>
-              <a className="px-5 py-2 rounded text-violet-500 hover:bg-white-100">Pricing</a>
-              <a className="px-5 py-2 rounded text-violet-500 hover:bg-white-100">Testimonials</a>
+              <a className="px-5 py-2 rounded text-violet-600 hover:bg-white-100">About</a>
+              <a className="px-5 py-2 rounded text-violet-600 hover:bg-white-100">Pricing</a>
+              <a className="px-5 py-2 rounded text-violet-600 hover:bg-white-100">Testimonials</a>
          
             </nav>
             <Link
@@ -53,7 +53,7 @@ const Hero = () => {
         <div className="flex flex-col items-center justify-center pt-10 mx-auto md:w-3/5">
           <h1 className="text-6xl font-extrabold text-center">
             <span className="block text-white">Build. Test. Sell.</span>
-            <span className="block text-violet-500">The Ultimate API Platform Is Here!</span>
+            <span className="block text-violet-600">The Ultimate API Platform Is Here!</span>
           </h1>
           
         </div>
